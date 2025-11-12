27f28082869be629bf8dfdd471b5f0abe5e86aea
Green  Billy <lgreen1567@gmail.com>
2025-11-12 10:02:21 -0500

    html et js pour les swiper

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,13 +1,28 @@
 import Icons from './utils/Icons.js';
 import Swiper from 'swiper/bundle';
 
+Icons.load();
+
 class Main {
   constructor() {
     this.init();
+    this.initSwiperPagination();
   }
 
   init() {
     Icons.load();
+  }
+
+  initSwiperPagination() {
+    const target = document.querySelector('.js-swiper-pagination');
+
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
